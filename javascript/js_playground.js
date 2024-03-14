f7f116cebc0533d5c0de652a115301e0d355a8fa
Oona Tiihonen <oonatiihonen@Oonas-MacBook-Air.local>
2024-03-14 20:17:00 +0100

    Played a bit with code

--- javascript/js_playground.js
+++ javascript/js_playground.js
@@ -1,6 +1,31 @@
-var name = "Jack";
+// var name = "Jack";
+// var age = 23; // in javascript everything is just a floating point number, whether it's shown or not
+
+// var message = "Hi, my name is " + name + " and I am " + age + " years old.";
+
+// console.log(message);
+
+var sum = 10 + 15; // "=" assignment operator; "+" addition operator
+var sub = 15 - 10; // "-" subtraction operator
+var mul = 10 * 3; // "*" multiplication operator
+var div = 9 / 3; // "/" division operator
+var mod = 10 % 3; // "%" modulus operator
+
+var msg = "10 / 3 = 3 with a remainder of " + mod;
+
+var result = 10 * 5 + 3 - 4; // 10 * 5 = 50; 50 + 3 = 53; 53 - 4 = 49
+console.log(sum)
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log(msg)
+console.log(result)
+
+var firstName = "John";
+var lastName = "Doe";
+var dateOfBirth = "10-09-82";
 var age = 23;
+var profileImgUrl = "http://coolpicks.com/johndoe.jpg";
 
-var message = "Hi, my name is " + name + " and I am " + age + " years old.";
-
-console.log(message);
+var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday!";
+console.log(loginWelcomeMessage);
