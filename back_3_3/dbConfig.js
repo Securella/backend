6ea871ea961564951d16183c65a3a158b41ccb34
Oona Tiihonen <tiihonen.oona@gmail.com>
2024-03-22 01:16:28 +0100

    Fixed Cannot GET/ issue

--- back_3_3/dbConfig.js
+++ back_3_3/dbConfig.js
@@ -1,11 +1,13 @@
-import { Pool } from 'pg';
+import pg from 'pg';
+
+const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: 'localhost',
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: 'localhost',
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
 });
 
 export default pool;
--- back_3_3/server.js
+++ back_3_3/server.js
@@ -9,6 +9,11 @@ app.use(express.json());
 // Routes
 app.use('/books', bookRouter);
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
